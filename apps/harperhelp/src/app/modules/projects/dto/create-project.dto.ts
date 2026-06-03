365091066971b448909a417e9761a47b647ea248
Hamza Shah <hamzagi345@gmail.com>
2026-06-03 20:32:58 +0500

    chore: Validator for create project dto

--- apps/harperhelp/src/app/modules/projects/dto/create-project.dto.ts
+++ apps/harperhelp/src/app/modules/projects/dto/create-project.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateProjectDto {
   @ApiProperty()
+  @IsString()
   name: string;
 
+  @IsString()
   @ApiProperty({ required: false })
   category: string;
 
+  @IsString()
   @ApiProperty({ required: false })
   brandColor: string;
 
+  @IsString()
   @ApiProperty({ required: false })
   logoLetter: string;
 }
