ca16090aab18ccf314895b79ff57b936e5e4fea0
epcprojects <endpointclients@gmail.com>
2026-07-21 01:18:50 +0500

    text changes.

--- apps/frontend/src/app/(auth)/login/LoginPageClient.tsx
+++ apps/frontend/src/app/(auth)/login/LoginPageClient.tsx
@@ -116,7 +116,7 @@ export default function LoginPageClient() {
         />
         <div className="md:flex-1 max-w-117 w-full flex items-center justify-center flex-col">
           <h2 className="text-black text-xl mb-6 md:mb-8 md:text-3xl font-bold text-center">
-            Welcome back ðŸ‘‹
+            Welcome back 👋
           </h2>
 
           <form
