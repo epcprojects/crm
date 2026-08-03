7607865a7b8a8b44978d85ceaa86763bcc3db3ab
epcprojects <isometric.mohsin@gmail.com>
2026-08-04 03:20:49 +0500

    minor fix

--- apps/frontend/src/app/(main-pages)/tickets/[ticketId]/page.tsx
+++ apps/frontend/src/app/(main-pages)/tickets/[ticketId]/page.tsx
@@ -1938,21 +1938,20 @@ export default function TicketDetailPage() {
                         ? isSendingChatMessage
                         : createReplyMutation.isPending
                     }
-                    // onSubmitReply={
-                    //   isInternalChatActive && canViewInternalChatBtn
-                    //     ? canPostReplies
-                    //       ? (payload) =>
-                    //           handleSubmitChatMessage({
-                    //             ...payload,
-                    //             channel: 'internal',
-                    //             sendMessage: sendInternalChatMessage,
-                    //           })
-                    //       : undefined
-                    //     : canPostReplies
-                    //       ? handleSubmitReply
-                    //       : undefined
-                    // }
-                    onSubmitReply={handleSubmitReply}
+                    onSubmitReply={
+                      isInternalChatActive && canViewInternalChatBtn
+                        ? canPostReplies
+                          ? (payload) =>
+                              handleSubmitChatMessage({
+                                ...payload,
+                                channel: 'internal',
+                                sendMessage: sendInternalChatMessage,
+                              })
+                          : undefined
+                        : canPostReplies
+                          ? handleSubmitReply
+                          : undefined
+                    }
                     requireMessage={false}
                     currentUserId={currentUserId}
                     onDeleteReply={
