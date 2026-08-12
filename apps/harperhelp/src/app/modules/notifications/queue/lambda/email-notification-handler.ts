ed74d392f810f54ffc06e70321e2663cc9b38062
Awais-Nazir <uetawais42@gmail.com>
2026-08-12 15:12:43 +0500

    added in lambda file

--- apps/harperhelp/src/app/modules/notifications/queue/lambda/email-notification-handler.ts
+++ apps/harperhelp/src/app/modules/notifications/queue/lambda/email-notification-handler.ts
@@ -71,6 +71,8 @@ function buildEmailForEvent(event: EmailNotificationEvent) {
       return buildProjectUnassignedEmail(event.payload, appUrl);
     case EmailEventType.THREAD_MESSAGE_CREATED:
       return buildThreadMessageCreatedEmail(event.payload, appUrl);
+    case EmailEventType.THREAD_REPLY_CREATED:
+      return buildThreadMessageCreatedEmail(event.payload, appUrl);
     case EmailEventType.TICKET_CREATED:
       return buildTicketCreatedEmail(event.payload, appUrl);
     case EmailEventType.TICKET_REPLY_POSTED:
@@ -97,6 +99,8 @@ function resolveRecipients(event: EmailNotificationEvent) {
     case EmailEventType.TICKET_CREATED:
       return event.payload.participants;
     case EmailEventType.THREAD_MESSAGE_CREATED:
+      return event.payload.participants;
+    case EmailEventType.THREAD_REPLY_CREATED:
       return event.payload.participants;
     case EmailEventType.TICKET_REPLY_POSTED:
       return event.payload.participants.filter(
